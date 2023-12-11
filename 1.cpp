277279adcc8a8dd2684e8cfce680a8bfc3a5e51c
Brian Mai <0hrs3g@gmail.com>
2023-12-11 15:07:53 -0800

    mod cnt

---
+++ 1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+private:
+
+public:
+    int threeOrFive() {
+        int res = 0;
+        for (int i = 0; i < 1000; ++i) {
+            res += (!(i % 3) || !(i % 5)) ? i : 0;
+        }
+        return res;
+    }
+};
+
+int main() {
+    Solution solution;
+    cout << solution.threeOrFive() << endl;
+    // 233168
+    return 0;
+}
