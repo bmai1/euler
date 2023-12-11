d3cb98a5e47d8aea4f8559931c5025728369501b
Brian Mai <0hrs3g@gmail.com>
2023-12-11 15:08:04 -0800

    template

---
+++ Solution.cpp
@@ -0,0 +1,17 @@
+// g++ -std=c++20 1.cpp -o 1.out && ./1.out
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+private:
+
+public:
+
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
